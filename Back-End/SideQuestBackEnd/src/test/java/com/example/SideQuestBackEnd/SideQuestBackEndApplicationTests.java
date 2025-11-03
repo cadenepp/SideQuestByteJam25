1a661367087352a9261276149023282d4b8b9e00
Caden Epp <ccepp2412@gmail.com>
2025-11-03 15:17:42 -0600

    initial commit

---
+++ Back-End/SideQuestBackEnd/src/test/java/com/example/SideQuestBackEnd/SideQuestBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SideQuestBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideQuestBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Front-End/side-quest-web/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Side Quest App</h1>
+    </div>
+  );
+}
